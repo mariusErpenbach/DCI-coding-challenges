393cb4bb6fa96b2af47f9669e3356afc4d42ef58
Hawty1 <49696027+Hawty1@users.noreply.github.com>
2021-01-13 10:58:36 +0100

    added lifetimesupply calculation
    with leapyears to be very exactly

--- january/13.01.2021.js
+++ january/13.01.2021.js
@@ -1,5 +1,7 @@
 // Round 2 coding challenges
 // repicking a task from yesterday and solve it in another way
+
+// Counting Occurrences
 function countOccurrences (string,letter) {  
     let occurrences = 0;                // set a var to count the occurences
   for (i=0;i<=string.length;i++){       // initialize a loop which runs as long as the string is
@@ -10,5 +12,6 @@ function countOccurrences (string,letter) {
   return occurrences;                   
   }
   console.log(countOccurrences("this is a stringi", "i"));
-
-//
+// Calculate the lifetimesupply but with leap years
+const calcLifetimeSupply = (age, supplyPerDay) => (((100-age)*365)*supplyPerDay)+Math.floor(25/4);  // Math.floor(25/4) adds the number of leap years to the calculation.
+console.log(calcLifetimeSupply(25,2));
