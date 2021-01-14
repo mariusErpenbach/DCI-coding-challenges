8bee374f912c1bb5f636fc8ab9b29dbe0921c4b8
Hawty1 <49696027+Hawty1@users.noreply.github.com>
2021-01-14 09:08:45 +0100

    added XO task

--- january/13.01.2021.js
+++ january/13.01.2021.js
@@ -1,7 +1,7 @@
-// Round 2 coding challenges
+/* Round 2 coding challenges
 // repicking a task from yesterday and solve it in another way
 
-// Counting Occurrences
+Counting Occurrences */
 function countOccurrences (string,letter) {  
     let occurrences = 0;                // set a var to count the occurences
   for (i=0;i<=string.length;i++){       // initialize a loop which runs as long as the string is
@@ -12,6 +12,20 @@ function countOccurrences (string,letter) {
   return occurrences;                   
   }
   console.log(countOccurrences("this is a stringi", "i"));
-// Calculate the lifetimesupply but with leap years
+
+  // Calculate the lifetimesupply but with leap years
 const calcLifetimeSupply = (age, supplyPerDay) => (((100-age)*365)*supplyPerDay)+Math.floor(25/4);  // Math.floor(25/4) adds the number of leap years to the calculation.
 console.log(calcLifetimeSupply(25,2));
+
+
+
+
+/* NEW TASK 
+11. XO Create a function that takes a string, checks if it has the same number of ’x’s and ’o’s and returns either true or false. Even if there are zero 'o's and 'x's it has to return true*/
+const isEqualNumXandO = (string) => {
+    let xOccurences = string.split("x").length-1 // count 'x'
+    let oOccurences = string.split("o").length-1 // count 'o'
+    if (xOccurences === oOccurences) { return true} // check if  'x' and 'o' have the same number in the string.
+    else return false;
+    }
+    console.log(isEqualNumXandO("sry"));
