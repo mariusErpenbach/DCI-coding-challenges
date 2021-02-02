042f09fc821f5bea5f7ed588f9910b7d306496e9
Hawty1 <49696027+Hawty1@users.noreply.github.com>
2021-02-02 09:16:21 +0100

    self invoking function

---
+++ february/01-02-20201.js
@@ -0,0 +1,11 @@
+
+((currentAge,retAge,monthWage,savingPerMonth)=>{
+  let yearsLeft = retAge-currentAge;
+  let monthLeft = yearsLeft*12;
+  let monthSafe = (monthWage/100)*savingPerMonth;
+  if (currentAge>=65){return "You're already retired!"}
+  else {
+    return console.log(`In your ${monthLeft} months left to live you can safe up to ${monthSafe*monthLeft}`);
+  }
+})(40,65,2000,5);
+
